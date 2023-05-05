01ab0141b595a23b7bbd51d0bab078f476041422
Vadim Gush <mail@vadimgush.com>
2023-05-05 18:40:55 +0400

    add constants

--- include/utils/types.h
+++ include/utils/types.h
@@ -19,6 +19,11 @@ using i8 = int8_t;
 using i16 = int16_t;
 using i32 = int32_t;
 using i64 = int64_t;
+
 using fd_t = int;
+
+constexpr static u32 KB = 1024;
+constexpr static u32 MB = 1024 * KB;
+constexpr static u32 GB = 1024 * MB;
 
 #endif //UTILITIES_TYPES_H
